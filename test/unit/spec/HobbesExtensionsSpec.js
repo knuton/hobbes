1492979af12bb94afb372d28f0b9cec0b289db84
Johannes Emerich <johannes@emerich.de>
2010-10-27 16:07:29 +0200

    Adjust test to reality

--- test/unit/spec/HobbesExtensionsSpec.js
+++ test/unit/spec/HobbesExtensionsSpec.js
@@ -2,7 +2,7 @@ describe('Extensions', function () {
 
   it('should create a prototypal heir', function () {
     var a = {iHasIt : true};
-    var b = Object.preate(a);
+    var b = Object.create(a);
     expect(b.iHasIt).toBeTruthy();
   });
 
