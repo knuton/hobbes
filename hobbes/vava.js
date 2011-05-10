3063e971479ca50e3e7d96d89db26688f5f74375
knuton <johannes@emerich.de>
2011-05-10 18:13:50 +0200

    Add automatic assembly of env

--- hobbes/vava.js
+++ hobbes/vava.js
@@ -1,17 +1,13 @@
+var utils = (typeof hobbes !== 'undefined' && hobbes.utils) || require('./utils');
+
 var vavaClass = require('./vava/class');
 var vavaMethod = require('./vava/method');
 var vavaType = require('./vava/type');
 
 exports.scope = require('./vava/scope');
 
-// TODO automate env assembly
-exports.env = {
-  VavaClass  : vavaClass.VavaClass,
-  VavaMethod : vavaMethod.VavaMethod,
-  TypedVariable : vavaType.TypedVariable,
-  BooleanValue : vavaType.BooleanValue,
-  IntValue   : vavaType.IntValue,
-  FloatValue   : vavaType.FloatValue,
-  DoubleValue   : vavaType.DoubleValue,
-  StringValue : vavaType.StringValue
-}
+exports.env = utils.merge(
+  vavaClass,
+  vavaMethod,
+  vavaType
+);
