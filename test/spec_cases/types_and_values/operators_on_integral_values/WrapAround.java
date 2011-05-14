b1c1c4a517f683075b3de937e978accfeb8bf478
knuton <johannes@emerich.de>
2011-05-14 18:53:03 +0200

    Add wrap around test and fix boundary detection

---
+++ test/spec_cases/types_and_values/operators_on_integral_values/WrapAround.java
@@ -0,0 +1,12 @@
+public class WrapAround {
+
+  public static void main(String[] args) {
+    byte a = 127;
+    byte b = 17;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(a+b);
+  }
+
+}
