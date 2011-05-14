03ac5571b74e70f1a1d321b2e1c9e7a915064c6c
knuton <johannes@emerich.de>
2011-05-14 15:21:55 +0200

    Add test for int to boolean conversion

---
+++ test/spec_cases/types_and_values/operators_on_boolean_values/IntegerToBoolean.java
@@ -0,0 +1,14 @@
+public class IntegerToBoolean {
+  public static void main(String[] args) {
+  
+    int fInt = 0;
+    boolean fBool = fInt != 0;
+
+    int tInt = 1;
+    boolean tBool = tInt != 0;
+
+    System.out.println(fBool);
+    System.out.println(tBool);
+
+  }
+}
